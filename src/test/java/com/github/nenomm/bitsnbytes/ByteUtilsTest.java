9e15d3bfdf52ce9ab9b8f3652584467b43d8500f
nenad <nenad.nenadovic@kapsch.net>
2018-02-20 08:22:07 +0100

    bit arithmetics demistified (somewhat)

--- src/test/java/com/github/nenomm/bitsnbytes/ByteUtilsTest.java
+++ src/test/java/com/github/nenomm/bitsnbytes/ByteUtilsTest.java
@@ -1,10 +1,14 @@
 package com.github.nenomm.bitsnbytes;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class ByteUtilsTest {
+	private static Logger log = LoggerFactory.getLogger(ByteUtilsTest.class);
 
 	@Test
 	public void testOneCounting() {
@@ -24,5 +28,84 @@ public class ByteUtilsTest {
 		assertEquals("00000000000000000000000000000100", ByteUtils.showBits(4));
 	}
 
-	// todo: test >> and << operators on two's complement values.
+	@Test
+	public void testComplements() {
+
+		// 20 -> -20
+		int number = 20;
+
+		assertEquals("00000000000000000000000000010100", ByteUtils.showBits(number));
+
+		number = ~number;
+		number = number + 1;
+
+		assertEquals(-20, number);
+
+		assertEquals("11111111111111111111111111101100", ByteUtils.showBits(number));
+
+		// -20 -> 20
+
+		number = ~number;
+		number = number + 1;
+
+		assertEquals(20, number);
+
+		assertEquals("00000000000000000000000000010100", ByteUtils.showBits(number));
+	}
+
+	@Test
+	public void testShifts() {
+		int number = 1;
+
+		assertEquals("00000000000000000000000000000001", ByteUtils.showBits(number));
+
+		number = ~number;
+		number = number + 1;
+
+		assertEquals(-1, number);
+		assertEquals("11111111111111111111111111111111", ByteUtils.showBits(number));
+
+		number = number >> 10;
+
+		assertEquals(-1, number);
+
+		number = number << 10;
+
+		assertNotEquals(-1, number);
+
+		number = -1;
+		number = number >>> 10;
+
+		assertNotEquals(-1, number);
+	}
+
+	@Test
+	public void testBitArithmetics() {
+		int number = 5;
+
+		assertEquals(40, number << 3);
+		assertEquals(9, 73 >> 3);
+
+		number = -5;
+		assertEquals(-40, number << 3);
+		assertEquals(-10, (-73) >> 3);    // -73 / 2 = -36.5 => -37 / 2 = -18.5 => -19 / 2 = -9.5 => -10
+
+		assertEquals(-37, (-73) >> 1);
+		assertEquals(-19, (-37) >> 1);
+		assertEquals(-10, (-19) >> 1);
+	}
+
+	@Test
+	public void contrivedProblemTest() {
+		int number = 0b1000_0000_0000_0000_0000_0000_0000_0000;
+		log.info("This special number is... {}", number);
+
+		log.info("Number: {}, negative number: {}", number - 1, -(number - 1));
+		log.info("Number: {}, negative number: {}", number, -number);
+		log.info("Number: {}, negative number: {}", number + 1, -(number + 1));
+
+		log.info("Min int is {}, max int is: {}", Integer.MIN_VALUE, Integer.MAX_VALUE);
+	}
+
+
 }
